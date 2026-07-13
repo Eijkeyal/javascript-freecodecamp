3431d486191d62e55dcd2b2de118f034f2e7542f
Eijkeyal <209868212+Eijkeyal@users.noreply.github.com>
2026-07-13 15:19:47 +0545

    Free Code Camp JavaScript Learning

---
+++ funFact.js
@@ -0,0 +1,29 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Eijkeyal";
+let botLocation = "Makawanpur";
+let favoriteLanguage = "Java";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+  favoriteLanguage + " was originally called Oak before it was renamed Java.";
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage + " follows the principle 'Write Once, Run Anywhere'.";
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage +
+  " is one of the most popular languages for backend development.";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! " +
+    botName +
+    " from " +
+    botLocation +
+    ".",
+);
