5f7e0b34862d8aa5bbc54ff02e767e9234e14773
Eijkeyal <209868212+Eijkeyal@users.noreply.github.com>
2026-07-15 15:33:41 +0545

    Free code camp JavaScript Learning

---
+++ buildLunchPicker.js
@@ -0,0 +1,50 @@
+const lunches = [];
+
+function addLunchToEnd(array, lunchItem) {
+  array.push(lunchItem);
+  console.log(`${lunchItem} added to the end of the lunch menu.`);
+  return array;
+}
+
+function addLunchToStart(array, lunchItem) {
+  array.unshift(lunchItem);
+  console.log(`${lunchItem} added to the start of the lunch menu.`);
+  return array;
+}
+
+function removeLastLunch(array) {
+  if (array.length === 0) {
+    console.log("No lunches to remove.");
+  } else {
+    const removed = array.pop();
+    console.log(`${removed} removed from the end of the lunch menu.`);
+  }
+  return array;
+}
+
+function removeFirstLunch(array) {
+  if (array.length === 0) {
+    console.log("No lunches to remove.");
+  } else {
+    const removed = array.shift();
+    console.log(`${removed} removed from the start of the lunch menu.`);
+  }
+  return array;
+}
+
+function getRandomLunch(array) {
+  if (array.length === 0) {
+    console.log("No lunches available.");
+  } else {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    console.log(`Randomly selected lunch: ${array[randomIndex]}`);
+  }
+}
+
+function showLunchMenu(array) {
+  if (array.length === 0) {
+    console.log("The menu is empty.");
+  } else {
+    console.log(`Menu items: ${array.join(", ")}`);
+  }
+}
